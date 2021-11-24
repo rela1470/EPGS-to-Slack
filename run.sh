79f2ddeef0e66c192e918e67d678ddb220f6c5f4
PiedHarrier <pajeromini2960@gmail.com>
2021-11-24 22:30:40 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -66,7 +66,7 @@ if [ $# = 1 ]; then
 
     # 予約関係: 追加, 削除, 更新, 録画準備
     if [ $ret = "reserve" ]; then
-        content="%0D%0A ✅ 予約追加 %0D%0A ${title} %0D%0A ${CHANNELTYPE} ${CHANNELNAME} %0D%0A ${startat}~${endat} %0D%0A 番組概要: %0D%0A ${description} %0D%0A ${extended}"
+        content="%0D%0A ✅ 予約追加 %0D%0A ${title} %0D%0A ${CHANNELTYPE} ${CHANNELNAME} %0D%0A ${startat}~${endat} %0D%0A %0D%0A ${description} %0D%0A ${extended}"
     elif [ $ret = "delete" ]; then
         content="%0D%0A 💨 予約削除 %0D%0A ${title} @ ${CHANNELTYPE} ${CHANNELNAME}"
     elif [ $ret = "update" ]; then
