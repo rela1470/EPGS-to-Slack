d794e8b3b05b14a077fe5f4ee089dd6c8e69c0c2
PiedHarrier <pajeromini2960@gmail.com>
2021-11-25 06:59:54 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -96,7 +96,7 @@ if [ $# = 1 ]; then
         else
             : # 何もしない
         fi
-        content="%0D%0A ⏹ 録画終了 %0D%0A ${title} @ ${CHANNELTYPE} ${CHANNELNAME} %0D%0A エラー: ${ERROR_CNT}, ドロップ: ${DROP_CNT}, スクランブル: ${SCRAMBLING_CNT}
+        content="%0D%0A ⏹ 録画終了 %0D%0A ${title} @ ${CHANNELTYPE} ${CHANNELNAME} %0D%0A エラー: ${ERROR_CNT}, ドロップ: ${DROP_CNT}, スクランブル: ${SCRAMBLING_CNT}"
     elif [ $ret = "recfailed" ]; then 
         # エラー, ドロップ, スクランブルカウントを読み込み
         if [ -z "$ERROR_CNT" ]; then
@@ -114,7 +114,7 @@ if [ $# = 1 ]; then
         else
             : # 何もしない
         fi
-        content="%0D%0A ❌ 録画失敗 %0D%0A ${title} @ ${CHANNELTYPE} ${CHANNELNAME} %0D%0A ${description} %0D%0A エラー: ${ERROR_CNT}, ドロップ: ${DROP_CNT}, スクランブル: ${SCRAMBLING_CNT}"
+        content="%0D%0A ❌ 録画失敗 %0D%0A ${title} @ ${CHANNELTYPE} ${CHANNELNAME} %0D%0A エラー: ${ERROR_CNT}, ドロップ: ${DROP_CNT}, スクランブル: ${SCRAMBLING_CNT}"
     else
         echo "引数が不正です。"
         exit 1
